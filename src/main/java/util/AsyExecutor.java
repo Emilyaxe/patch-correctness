47d931dd47964cb28aab3d18634c695c5e8bb241
houyaozong <houyaozong@kuaishou.com>
2021-05-09 14:36:17 +0800

    finish mul thread

--- src/main/java/util/AsyExecutor.java
+++ src/main/java/util/AsyExecutor.java
@@ -14,11 +14,11 @@ public class AsyExecutor {
 
     public static final ExecutorService EXECUTOR = new ThreadPoolExecutor(THREAD_NUM, THREAD_NUM,
             10, TimeUnit.SECONDS, new LinkedBlockingQueue<>(1000), r -> {
-        Thread t = new Thread(r);
-        t.setDaemon(true);
-        t.setName("process-subject");
-        return t;
-    },
+                Thread t = new Thread(r);
+                t.setDaemon(true);
+                t.setName("process-subject-%d");
+                return t;
+            },
             (runnable, currentExecutor) -> log.warn(
                     "rejectedExecution mul thread Task {} rejected from {}", runnable.toString(),
                     currentExecutor.toString()));
