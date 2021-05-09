578c47e73edf29f6f9826454ceaf8294d27b849e
houyaozong <houyaozong@kuaishou.com>
2021-05-09 13:46:29 +0800

    finish mul thread

--- src/main/java/script/MulThreadTest.java
+++ src/main/java/script/MulThreadTest.java
@@ -19,9 +19,13 @@ public class MulThreadTest {
 
     public static final int THREAD_NUM = 2 * Runtime.getRuntime().availableProcessors();
 
-    public static final ExecutorService EXECUTOR = new ThreadPoolExecutor(THREAD_NUM,
-            THREAD_NUM, 10, TimeUnit.SECONDS,
-            new LinkedBlockingQueue<>(1000),
+    public static final ExecutorService EXECUTOR = new ThreadPoolExecutor(THREAD_NUM, THREAD_NUM,
+            10, TimeUnit.SECONDS, new LinkedBlockingQueue<>(1000), r -> {
+                Thread t = new Thread(r);
+                t.setDaemon(true);
+                t.setName("process-subject");
+                return t;
+            },
             (runnable, currentExecutor) -> log.warn(
                     "rejectedExecution mul thread Task {} rejected from {}", runnable.toString(),
                     currentExecutor.toString()));
