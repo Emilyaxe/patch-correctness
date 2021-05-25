2548fcf71fbe20a7bddb196cd82b0b62c4584d37
Emilyaxe <1260901930@qq.com>
2021-05-25 21:23:05 +0800

    update code

--- src/main/java/service/CheckResult.java
+++ src/main/java/service/CheckResult.java
@@ -1,6 +1,5 @@
 package service;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
@@ -37,37 +36,40 @@ public class CheckResult {
         List<PatchJson> correctFileList = JSON.parseArray(FileIO.readFileToString(correctFile), PatchJson.class);
         correctFileList.forEach(patchJson -> patchJson
                 .setPatchPath(Constant.HOME + "/Patches/DataSet/correctSet/" + patchJson.getPatchName()));
+        List<PatchJson> patchJsons = Stream.of(testFileList, trainFileList, correctFileList)
+                .flatMap(List::stream).collect(Collectors.toList());
 
+        log.info("All Patches: {}", patchJsons.size());
         Map<String, PatchJson> patchMap =
                 Stream.of(testFileList, trainFileList, correctFileList)
                         .flatMap(List::stream).collect(
                         Collectors.toMap(PatchJson::getPatchName, Function.identity(), (v1, v2) -> v2));
 
-        String[] problemPatchList = problemPatches.split(",");
+        //String[] problemPatchList = problemPatches.split(",");
 
         // 1. all tests have no trace
-        String allTestNoTrace = Arrays.stream(problemPatchList).map(patchMap::get).filter(Objects::nonNull)
+        String allTestNoTrace = patchJsons.stream().filter(Objects::nonNull)
                 .filter(patchJson -> MapUtils.isEmpty(patchJson.getBuggyTraceInfo())).map(PatchJson::getPatchName)
                 .collect(Collectors.joining(","));
         log.error("all test have no trace on buggy version: {}", allTestNoTrace);
 
         // 2. all  tests have no trace
-        String allTestNoTraceFixed = Arrays.stream(problemPatchList).map(patchMap::get).filter(Objects::nonNull)
+        String allTestNoTraceFixed = patchJsons.stream().filter(Objects::nonNull)
                 .filter(patchJson -> MapUtils.isEmpty(patchJson.getFixedTraceInfo())).map(PatchJson::getPatchName)
                 .collect(Collectors.joining(","));
         log.error("all test have no trace on fixed version: {}", allTestNoTraceFixed);
 
 
-        // 1. all tests have no trace
-        String allFailingTestNoTrace = Arrays.stream(problemPatchList).map(patchMap::get).filter(Objects::nonNull)
+        // 1. all failing tests have no trace
+        String allFailingTestNoTrace = patchJsons.stream().filter(Objects::nonNull)
                 .filter(patchJson -> patchJson.getFailingTests().stream()
                         .noneMatch(test -> patchJson.getBuggyTraceInfo().containsKey(test)))
                 .map(PatchJson::getPatchName)
                 .collect(Collectors.joining(","));
         log.error("all falling test have no trace on buggy version: {}", allFailingTestNoTrace);
 
-        // 2. all  tests have no trace
-        String allFailingTestNoTraceFixed = Arrays.stream(problemPatchList).map(patchMap::get).filter(Objects::nonNull)
+        // 2. all  failing tests have no trace
+        String allFailingTestNoTraceFixed = patchJsons.stream().filter(Objects::nonNull)
                 .filter(patchJson -> patchJson.getFailingTests().stream()
                         .noneMatch(test -> patchJson.getFixedTraceInfo().containsKey(test)))
                 .map(PatchJson::getPatchName)
@@ -75,16 +77,32 @@ public class CheckResult {
         log.error("all falling test have no trace on fixed version: {}", allFailingTestNoTraceFixed);
 
 
+        // 1. all passing tests have no trace
+        String allPassingTestNoTrace = patchJsons.stream().filter(Objects::nonNull)
+                .filter(patchJson -> patchJson.getBuggyTraceInfo().keySet().stream()
+                        .allMatch(test -> patchJson.getFailingTests().contains(test)))
+                .map(PatchJson::getPatchName)
+                .collect(Collectors.joining(","));
+        log.error("all passing test have no trace on buggy version: {}", allPassingTestNoTrace);
+
+        // 2. all  passing tests have no trace
+        String allPassingTestNoTraceFixed = patchJsons.stream().filter(Objects::nonNull)
+                .filter(patchJson -> patchJson.getFixedTraceInfo().keySet().stream()
+                        .allMatch(test -> patchJson.getFailingTests().contains(test)))
+                .map(PatchJson::getPatchName)
+                .collect(Collectors.joining(","));
+        log.error("all passing test have no trace on fixed version: {}", allPassingTestNoTraceFixed);
+
         // 3. patch has multiple modification
         String multipleModification =
-                Arrays.stream(problemPatchList).map(patchMap::get).filter(Objects::nonNull)
+                patchJsons.stream().filter(Objects::nonNull)
                         .filter(patchJson -> FileIO.readFileToString(patchJson.getPatchPath()).split("--- ").length > 1)
                         .map(PatchJson::getPatchName).collect(Collectors.joining(","));
         log.error("multiple modification test have no trace: {}", multipleModification);
 
         // 4. other
         // failing test has no trace
-        String other = Arrays.stream(problemPatchList).map(patchMap::get).filter(Objects::nonNull)
+        String other = patchJsons.stream().map(patchMap::get).filter(Objects::nonNull)
                 .filter(patchJson -> patchJson.getBuggyTraceInfo().keySet().containsAll(patchJson.getFailingTests())
                         && patchJson.getFixedTraceInfo().keySet().containsAll(patchJson.getFailingTests()))
                 .map(patchJson -> JSON
@@ -99,34 +117,5 @@ public class CheckResult {
         mainProcess();
     }
 
-    public static String problemPatches =
-            "Math_77.src.patch,Chart14b_Patch188,patch1-Chart-4-SOFix.patch,Math_39.src.patch,Lang_36.src.patch,"
-                    + "patch1-Lang-10-jKali.patch,patch1-Chart-4-kPAR.patch,patch1-Math-35-ACS.patch,Chart_19.src"
-                    + ".patch,patch1-Chart-19-AVATAR.patch,Math35b_Patch199,Lang_20.src.patch,"
-                    + "patch1-Math-6-AVATAR-plausible.patch,patch1-Lang-59-SOFix.patch,Math69b_Patch169,Math_31.src"
-                    + ".patch,Lang_50.src.patch,patch1-Math-80-Arja-plausible.patch,patch1-Lang-57-TBar.patch,"
-                    + "patch1-Math-35-Arja-plausible.patch,Closure_79.src.patch,Closure_47.src.patch,Math_1.src"
-                    + ".patch,Math_41.src.patch,Time_22.src.patch,Math_54.src.patch,patch1-Math-41-SimFix.patch,"
-                    + "Lang_15.src.patch,patch1-Lang-57-kPAR-plausible.patch,Closure_46.src.patch,Math_102.src.patch,"
-                    + "patch1-Math-4-AVATAR.patch,patch1-Chart-25-TBar-plausible.patch,patch1-Math-4-ACS-plausible"
-                    + ".patch,patch1-Chart-25-Arja-plausible.patch,patch1-Math-29-jKali-plausible.patch,Lang_60.src"
-                    + ".patch,patch1-Math-20-FixMiner-plausible.patch,patch1-Math-6-SimFix-plausible.patch,"
-                    + "patch1-Math-53-Jaid.patch,Lang_35.src.patch,Math41b_Patch162,patch1-Lang-7-Arja-plausible"
-                    + ".patch,patch1-Lang-10-kPAR-plausible.patch,patch1-Math-15-kPAR-plausible.patch,Lang_42.src"
-                    + ".patch,Math_37.src.patch,Chart19b_Patch189,patch1-Math-69-Nopol-plausible.patch,"
-                    + "patch1-Math-41-DynaMoth-plausible.patch,patch1-Lang-57-AVATAR.patch,Lang_47.src.patch,"
-                    + "patch1-Lang-13-AVATAR-plausible.patch,Chart_8.src.patch,patch1-Chart-14-Arja-plausible.patch,"
-                    + "Closure_106.src.patch,Time_6.src.patch,Lang_10.src.patch,patch1-Closure-46-AVATAR.patch,"
-                    + "patch1-Lang-20-Arja-plausible.patch,patch1-Lang-10-TBar.patch,Math_36.src.patch,Math_6.src"
-                    + ".patch,Closure_60.src.patch,patch2-Lang-57-CapGen.patch,Math4b_Patch195,Closure_89.src.patch,"
-                    + "Time_12.src.patch,patch1-Chart-14-ACS.patch,patch1-Lang-57-CapGen.patch,Lang_8.src.patch,"
-                    + "Chart_2.src.patch,Math_22.src.patch,Math_29.src.patch,Math_67.src.patch,Math_35.src.patch,"
-                    + "Math28b_Patch158,Lang57b_PatchHDRepair1,Chart_22.src.patch,Lang_30.src.patch,Closure_27.src"
-                    + ".patch,patch1-Math-41-Nopol-plausible.patch,Math_49.src.patch,Lang_41.src.patch,Math_15.src"
-                    + ".patch,Chart_25.src.patch,Math_4.src.patch,patch1-Chart-8-CapGen.patch,"
-                    + "patch1-Lang-35-ACS-plausible.patch,patch1-Math-15-TBar-plausible.patch,Math_71.src.patch,"
-                    + "Lang35b_Patch192,Math_101.src.patch,Chart_15.src.patch,patch1-Math-31-GenProg-plausible.patch,"
-                    + "Closure_80.src.patch,patch1-Chart-15-TBar-plausible.patch,Chart_4.src.patch,Math_69.src.patch,"
-                    + "patch1-Chart-12-GenProg-plausible.patch,Lang_9.src.patch,Math_43.src.patch,Time_3.src.patch,"
-                    + "patch3-Lang-57-CapGen.patch";
+
 }
