86a2aeaeafd9cc64745bdb402f40993cf3726d54
houyaozong <houyaozong@kuaishou.com>
2021-05-25 13:33:30 +0800

    finish mul thread

--- src/main/java/service/BuildJsonResult.java
+++ src/main/java/service/BuildJsonResult.java
@@ -2,11 +2,7 @@ package service;
 
 import static util.AsyExecutor.EXECUTOR;
 
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.LinkedHashMap;
 import java.util.LinkedHashSet;
@@ -20,6 +16,8 @@ import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.stream.Collectors;
 
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.LineIterator;
 import org.apache.commons.lang3.StringUtils;
 
 import com.alibaba.fastjson.JSON;
@@ -33,14 +31,14 @@ import util.FileIO;
 
 @Slf4j
 public class BuildJsonResult {
+
     public static Map<String, String> failingTestProblemList = new ConcurrentHashMap<>();
     public static Map<String, String> traceProblemList = new ConcurrentHashMap<>();
 
-
     public static void BuildPatchJson(String dir) {
         List<PatchJson> patches = new LinkedList<>();
-        JSONObject jsonObject =
-                JSON.parseObject(FileIO.readFileToString(BuildPath.buildMethodReFile(dir)));
+        JSONObject jsonObject = JSON
+                .parseObject(FileIO.readFileToString(BuildPath.buildMethodReFile(dir)));
         log.info("Obtain Static Info ...");
         for (Entry<String, Object> entry : jsonObject.entrySet()) {
             String patchName = entry.getKey();
@@ -52,9 +50,8 @@ public class BuildJsonResult {
             } else {
                 log.error(entry.getValue().toString());
             }
-            patches.add(
-                    PatchJson.builder().patchName(patchName).bugId(bugid).label(label).combinedMethod(combinedMethod)
-                            .build());
+            patches.add(PatchJson.builder().patchName(patchName).bugId(bugid).label(label)
+                    .combinedMethod(combinedMethod).build());
         }
         log.info("Obtain Dynamic Info ...");
         List<CompletableFuture<Void>> completableFutures = new LinkedList<>();
@@ -65,28 +62,26 @@ public class BuildJsonResult {
             completableFutures.add(CompletableFuture.runAsync(() -> {
                 log.info("Patch {} dynamic info collecting ...", patchJson.getPatchName());
                 String buggyLine = BuildPath.buildDymicAllFile(dir, patchJson.getPatchName(), true);
-                String fixedLine = BuildPath.buildDymicAllFile(dir, patchJson.getPatchName(), false);
+                String fixedLine = BuildPath.buildDymicAllFile(dir, patchJson.getPatchName(),
+                        false);
                 String failingTestContent = FileIO.readFileToString(
-                        BuildPath.buildDymicAllFile(dir, patchJson.getPatchName(), true) + ".failing");
+                        BuildPath.buildDymicAllFile(dir, patchJson.getPatchName(), true)
+                                + ".failing");
                 patchJson.setFailingTests(
                         Arrays.stream(failingTestContent.split("\n")).filter(Objects::nonNull)
-                                .filter(StringUtils::isNotBlank)
-                                .collect(Collectors.toSet()));
-                String passingTest =
-                        FileIO.readFileToString(Constant.PROJ_INFO + "/passing_tests/" + patchJson.getBugId()
-                                .split("-")[0] + "/" + patchJson.getBugId().split("-")[1]);
+                                .filter(StringUtils::isNotBlank).collect(Collectors.toSet()));
+                String passingTest = FileIO.readFileToString(
+                        Constant.PROJ_INFO + "/passing_tests/" + patchJson.getBugId().split("-")[0]
+                                + "/" + patchJson.getBugId().split("-")[1]);
                 Set<String> testSet = new LinkedHashSet<>(Arrays.asList(passingTest.split("\n")));
                 testSet.addAll(patchJson.getFailingTests());
-
 
                 Map<String, Set<String>> buggyMap = null, fixedMap = null;
                 if (new File(buggyLine).length() / 1024.0 / 1024.0 / 1024.0 > 3.0) {
                     try {
                         buggyMap = obtainTraceByFile(buggyLine, testSet);
-                    } catch (IOException e) {
-                        e.printStackTrace();
                     } catch (OutOfMemoryError e) {
-                        traceProblemList.put(patchJson.getPatchName(), "");
+                        e.printStackTrace();
                     }
                 } else {
                     try {
@@ -98,10 +93,8 @@ public class BuildJsonResult {
                 if (new File(fixedLine).length() / 1024.0 / 1024.0 / 1024.0 > 3.0) {
                     try {
                         fixedMap = obtainTraceByFile(fixedLine, testSet);
-                    } catch (IOException e) {
-                        e.printStackTrace();
                     } catch (OutOfMemoryError e) {
-                        traceProblemList.put(patchJson.getPatchName(), "");
+                        e.printStackTrace();
                     }
                 } else {
                     try {
@@ -122,62 +115,61 @@ public class BuildJsonResult {
     }
 
     private static Map<String, Set<String>> obtainTraceByFile(String file, Set<String> testSet)
-            throws IOException, OutOfMemoryError {
-
+            throws OutOfMemoryError {
         Map<String, Set<String>> map = new LinkedHashMap<>();
-        FileInputStream inputStream = new FileInputStream(file);
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
-
-        String line = null;
-        while ((line = bufferedReader.readLine()) != null) {
-            //System.out.println(str);
-            if (StringUtils.isEmpty(line.trim())) {
-                continue;
-            }
-            String[] lineArray = line.split("\t", 2);
-            String key = lineArray[0].split("#")[0];
-            if (StringUtils.isBlank(checkTest(testSet, key))) {
-                continue;
-            }
-            if (StringUtils.isBlank(lineArray[1])) {
-                boolean find = false;
-                String str = null;
-                while ((str = bufferedReader.readLine()) != null) {
-
-                    String[] currentLineArray = str.split("\t", 2);
-                    String currentKey = checkTest(testSet, currentLineArray[0].split("#")[0]);
-                    if (StringUtils.isNotBlank(currentKey)) {
-                        break;
-                    } else {
-                        if (StringUtils.isBlank(currentLineArray[1])) {
-                            continue;
-                        } else {
-                            find = true;
+        LineIterator it = null;
+        try {
+            it = FileUtils.lineIterator(new File(file), "UTF-8");
+            String line = null;
+            while (it.hasNext()) {
+                //System.out.println(str);
+                line = it.nextLine();
+                if (StringUtils.isEmpty(line.trim())) {
+                    continue;
+                }
+                String[] lineArray = line.split("\t", 2);
+                String key = lineArray[0].split("#")[0];
+                if (StringUtils.isBlank(checkTest(testSet, key))) {
+                    continue;
+                }
+                if (StringUtils.isBlank(lineArray[1])) {
+                    boolean find = false;
+                    String str = null;
+                    while (it.hasNext()) {
+                        str = it.nextLine();
+                        String[] currentLineArray = str.split("\t", 2);
+                        String currentKey = checkTest(testSet, currentLineArray[0].split("#")[0]);
+                        if (StringUtils.isNotBlank(currentKey)) {
                             break;
+                        } else {
+                            if (StringUtils.isNotBlank(currentLineArray[1])) {
+                                find = true;
+                                break;
+                            }
                         }
                     }
-                }
-                if (find) {
-                    Set<String> values = Arrays.stream(line.split("\t", 2)[1].split("\t")).filter(Objects::nonNull)
-                            .filter(StringUtils::isNotBlank).collect(
-                                    Collectors.toSet());
+                    if (find) {
+                        Set<String> values = Arrays.stream(line.split("\t", 2)[1].split("\t"))
+                                .filter(Objects::nonNull).filter(StringUtils::isNotBlank)
+                                .collect(Collectors.toSet());
+                        if (values.size() > 0) {
+                            map.put(checkTest(testSet, key), values);
+                        }
+                    }
+                } else {
+                    Set<String> values = Arrays.stream(line.split("\t", 2)[1].split("\t"))
+                            .filter(Objects::nonNull).filter(StringUtils::isNotBlank)
+                            .collect(Collectors.toSet());
                     if (values.size() > 0) {
                         map.put(checkTest(testSet, key), values);
                     }
                 }
-            } else {
-                Set<String> values =
-                        Arrays.stream(line.split("\t", 2)[1].split("\t")).filter(Objects::nonNull)
-                                .filter(StringUtils::isNotBlank).collect(
-                                Collectors.toSet());
-                if (values.size() > 0) {
-                    map.put(checkTest(testSet, key), values);
-                }
             }
+        } catch (Exception e) {
+            log.error("file {} obtain trace failed!", file, e);
+        } finally {
+            LineIterator.closeQuietly(it);
         }
-        //close
-        inputStream.close();
-        bufferedReader.close();
         return map;
     }
 
@@ -192,7 +184,6 @@ public class BuildJsonResult {
     }
 
     private static void checkPatches(List<PatchJson> patchJsons) {
-
 
         for (PatchJson patchJson : patchJsons) {
             //            if (!patchJson.getPatchName().equals("patch1-Lang-10-kPAR-plausible.patch")) {
@@ -211,22 +202,24 @@ public class BuildJsonResult {
             //            }
             String combineMethod = patchJson.getCombinedMethod();
             if (!(checkMapTrace(combineMethod,
-                    buggyMap.values().stream().flatMap(Set::stream).collect(Collectors.toSet()), true) &&
-                    checkMapTrace(combineMethod,
-                            fixedMap.values().stream().flatMap(Set::stream).collect(Collectors.toSet()), false))) {
+                    buggyMap.values().stream().flatMap(Set::stream).collect(Collectors.toSet()),
+                    true)
+                    && checkMapTrace(combineMethod, fixedMap.values().stream().flatMap(Set::stream)
+                            .collect(Collectors.toSet()), false))) {
                 log.error("Patch {} has a wrong map", patchJson.getPatchName());
                 traceProblemList.put(patchJson.getPatchName(), "");
             }
         }
     }
 
-    private static boolean checkMapTrace(String combinedMethod, Set<String> trace, boolean isbuggy) {
+    private static boolean checkMapTrace(String combinedMethod, Set<String> trace,
+            boolean isbuggy) {
         String[] methods = combinedMethod.split("\n");
         String illegalStart = isbuggy ? "+" : "-";
         for (String line : trace) {
             int lineNumber = Integer.parseInt(line.split("#")[1]);
-            if (lineNumber >= methods.length || methods[lineNumber].startsWith("//") || methods[lineNumber]
-                    .startsWith(illegalStart)) {
+            if (lineNumber >= methods.length || methods[lineNumber].startsWith("//")
+                    || methods[lineNumber].startsWith(illegalStart)) {
                 return false;
             }
         }
@@ -271,18 +264,17 @@ public class BuildJsonResult {
                     }
                 }
                 if (find) {
-                    Set<String> values = Arrays.stream(line.split("\t", 2)[1].split("\t")).filter(Objects::nonNull)
-                            .filter(StringUtils::isNotBlank).collect(
-                                    Collectors.toSet());
+                    Set<String> values = Arrays.stream(line.split("\t", 2)[1].split("\t"))
+                            .filter(Objects::nonNull).filter(StringUtils::isNotBlank)
+                            .collect(Collectors.toSet());
                     if (values.size() > 0) {
                         map.put(checkTest(testSet, key), values);
                     }
                 }
             } else {
-                Set<String> values =
-                        Arrays.stream(line.split("\t", 2)[1].split("\t")).filter(Objects::nonNull)
-                                .filter(StringUtils::isNotBlank).collect(
-                                Collectors.toSet());
+                Set<String> values = Arrays.stream(line.split("\t", 2)[1].split("\t"))
+                        .filter(Objects::nonNull).filter(StringUtils::isNotBlank)
+                        .collect(Collectors.toSet());
                 if (values.size() > 0) {
                     map.put(checkTest(testSet, key), values);
                 }
@@ -308,7 +300,8 @@ public class BuildJsonResult {
         BuildPatchJson("testSet");
         BuildPatchJson("correctSet");
 
-        log.info("failingTestProblemList: {}", StringUtils.join(failingTestProblemList.keySet(), ","));
+        log.info("failingTestProblemList: {}",
+                StringUtils.join(failingTestProblemList.keySet(), ","));
         log.info("traceProblemList: {}", StringUtils.join(traceProblemList.keySet(), ","));
         //        String trainStatic = FileIO.readFileToString(BuildPath.buildMethodReFile("trainSet"));
         //        String correctStatic = FileIO.readFileToString(BuildPath.buildMethodReFile("correctSet"));
